7333306e3043e288466a3fb2066a9062fe70b7cb
TonyHomsi <tonyhomsi87@gmail.com>
2021-01-28 13:51:33 +0100

    dynamic content in React

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
-import React, {Component} from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-class App extends Component{
+class App extends Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
-      string: 'Hello Tony Homsi'
-    }
+      monster: [
+        {
+          name: "Frankenstein",
+          id: 'as1'
+        },
+        {
+          name: "Dracula",
+          id: 'as2'
+        },
+        {
+          name: "Zombie",
+          id: 'as3'
+        },
+      ],
+    };
   }
-  render(){
+  render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            { this.state.string}
-          </p>
-          <button onClick={() => this.setState({ string: 'Hello EveryOne!'})}>
-            Change Text
-          </button>
-        </header>
+        {this.state.monster.map((monster) => (
+          <h1 key={monster.id}> {monster.name}</h1>
+        ))}
       </div>
     );
   }
 }
-export default App
+export default App;
