4d13cca00c097215638a70566546bddcf593de19
TonyHomsi <tonyhomsi87@gmail.com>
2021-01-29 11:12:58 +0100

    Adding card-list.component.jsx/css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { CardList } from "./components/card-list/card-list.component";
 import "./App.css";
 
 class App extends Component {
@@ -6,23 +7,24 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-      ],
+      monsters: [],
     };
   }
 
-componentDidMount(){
-  fetch('https://jsonplaceholder.typicode.com/users')
-  .then(response => response.json())
-  .then(users =>this.setState({ monsters: users}))
-}
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
 
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => (
-          <h1 key={monster.id }> {monster.name}</h1>
-        ))}
+        <CardList name="Tony">
+          {this.state.monsters.map((monster) => (
+            <h1 key={monster.id}> {monster.name}</h1>
+          ))}
+        </CardList>
       </div>
     );
   }
