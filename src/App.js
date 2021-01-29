fbb0dc7703001381ee1eb3c0f76e5a10d565fe3a
TonyHomsi <tonyhomsi87@gmail.com>
2021-01-29 17:55:58 +0100

    SearchField State

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: "",
     };
   }
 
@@ -20,11 +21,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CardList name="Tony">
-          {this.state.monsters.map((monster) => (
-            <h1 key={monster.id}> {monster.name}</h1>
-          ))}
-        </CardList>
+        <input
+          type="search"
+          placeholder="search monster"
+          onChange={e => 
+            this.setState({ searchField: e.target.value }, () =>
+              console.log(this.state)
+            )
+          }
+        />
+        <CardList monsters={this.state.monsters}></CardList>
       </div>
     );
   }
